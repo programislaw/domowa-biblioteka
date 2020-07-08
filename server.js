feb672982ed8acea1eecae4a71431f1648b3742a
bak <Slawomir.Bak@smartstream-stp.com>
2020-07-08 22:57:21 +0200

    run fix

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const proxy = require('http-proxy-middleware')
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/domowa-biblioteka'));
+
+var apiProxy = proxy('/landing', {target: 'http://domowa-biblioteka.herokuapp.com', changeOrigin: true });
+app.use(apiProxy)
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/domowa-biblioteka/index.html'));
+});
+
+// default Heroku PORT
+app.listen(process.env.PORT || 80);
